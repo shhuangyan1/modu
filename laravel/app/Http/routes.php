dc0b8d712f5649f204a9eb9c98d52ce9930c708c
PC-20170726TMTN\Administrator <469688010@qq.com>
2017-08-21 18:37:17 +0800

    路由

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -22,18 +22,20 @@ Route::get('jiekou','Admin\LoginController@jiekou');
 Route::get('admin/confirm','Admin\ArticleController@confirm');
 Route::any('admin/shenhe','Admin\ArticleController@shenhe');
 
+
 Route::group(['middleware' => ['web','admin.login'],'prefix'=>'admin','namespace'=>'Admin'], function () {
 
     Route::get('index','IndexController@index');
     Route::get('info','IndexController@info');
     Route::get('logout','LoginController@logout');
-
     Route::resource('category','CategoryController');
     Route::resource('article','ArticleController');
     Route::resource('topic','TopicController');
     Route::resource('activity','ActivityController');
     Route::resource('manager','ManagerController');
     Route::any('upload','ActivityController@uploadvideo');
+    Route::any('stoptopic','TopicController@stoptopic');
+    //Route::any('cancelactivity','ActivityController@cancelactivity');
 
 
 });
