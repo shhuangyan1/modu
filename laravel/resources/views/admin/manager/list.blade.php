6555228cb4556105b2a3ccb6d6cfe00928c80c32
PC-20170726NIEA\Administrator <bragg_chen@163.com>
2017-10-18 10:18:53 +0800

    delete

--- laravel/resources/views/admin/manager/list.blade.php
+++ laravel/resources/views/admin/manager/list.blade.php
@@ -5,10 +5,11 @@
     <title>管理员列表</title>
     <link rel="stylesheet" href="{{asset('admin/style/css/ch-ui.admin.css')}}">
     <link rel="stylesheet" href="{{asset('admin/style/font/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('lirary/jedate/skin/default.css')}}">
+
     <script src="{{asset('lirary/uploadify/jquery1.11.3.min.js')}}" type="text/javascript"></script>
     <script type="text/javascript" src="{{asset('js/util.js')}}"></script>
-
-
+    <script src="{{asset('lirary/jedate/jquery.jebox.js')}}" ></script>
 
 </head>
 <body>
@@ -47,12 +48,52 @@
                 <td>
                     {{$v->auth_id}}
                 </td>
-                <td class="tc"><a href="#">删除</a></td>
+                <td class="tc"><a data-id="{{$v->id}}" class="delete">删除</a></td>
             </tr>
             @endforeach
             </tbody>
         </table>
     </section>
 </div>
+<script>
+    $(function () {
+        $(document).on("click",".delete",function () {
+            var id = $(this).data("id");
+            var this_ = $(this)
+            jeBox.open({
+                cell:"jbx",
+                title:"删除",
+                padding:"25px 10px",
+                content:'<div class="jeBox-iconbox jeicon1">确定要删除该管理员吗？</div>',
+                maskLock : true ,
+                btnAlign:"center",
+                button:[
+                    {
+                        name: '确定',
+                        callback: function(index){
+                            MD.ajax_get({
+                                url: 'admin/manager/',
+                                data: {"id" : id}
+                            },function (res) {
+                                jeBox.close(index)
+                                jeBox.msg('删除成功', {icon: 1,time:1});
+                                setTimeout(function () {
+                                    location.reload()
+                                },800)
+                            },function () {
+                                jeBox.close(index)
+                                jeBox.msg('操作失败', {icon: 3,time:1.5});
+                            })
+                        }
+                    },
+                    {
+                        name: '取消'
+                    }
+                ]
+            })
+
+        })
+    })
+</script>
 </body>
 </html>
