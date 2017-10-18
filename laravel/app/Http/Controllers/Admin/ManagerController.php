53dddf0fac3abd39d647b087f48d22dbaf8faca4
shhuangyan1 <shhuangyan1@163.com>
2017-10-18 09:27:37 +0800

    no message

--- laravel/app/Http/Controllers/Admin/ManagerController.php
+++ laravel/app/Http/Controllers/Admin/ManagerController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Model\Manager;
 use App\Http\Controllers\Controller;
-
+use DB;
 class ManagerController extends Controller
 {
     /**
