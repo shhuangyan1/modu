4e9d90423d69b46ab13ac249b0242bb389690ae3
PC-20170726NIEA\Administrator <18321941335@163.com>
2018-02-07 16:10:42 +0800

    ..

--- laravel/resources/views/admin/temact/address.blade.php
+++ laravel/resources/views/admin/temact/address.blade.php
@@ -106,113 +106,13 @@
 <!--搜索结果页面 列表 结束-->
 <script>
     $(function () {
+        $.ajax({
+            url:'{url("admin/pingguo/showaddr")}',
+            dataType: json,
+            success: function (res) {
+                console.log(res);
+            }
 
-        $(document).on("click",".to-delete",function () {
-            var id = $(this).data("id");
-            var this_ = $(this)
-            jeBox.open({
-                cell:"jbx",
-                title:"申请删除",
-                boxSize:["300px","160px"],
-                padding:"25px 10px",
-                content:'<p style="text-align: center;">确定要提交删除申请吗？</p>',
-                maskLock : true ,
-                btnAlign:"center",
-                button:[
-                    {
-                        name: '确定',
-                        callback: function(index){
-                            $.post(
-                                MD.url + "/admin/shenhe",
-                                {'_token':"{{csrf_token()}}","id":id},
-                                function (res) {
-                                    res = JSON.parse(res);
-                                    if(res.status == 1001){
-                                        jeBox.msg(res.msg, {icon: 3,time:1.5});
-//                                        error
-                                    }else{
-                                        jeBox.msg("申请已提交", {icon: 2,time:1.5});
-                                        this_.removeClass("to-delete").text("已申请").css("color","#f40")
-                                    }
-                                    jeBox.close(index);
-                                }
-                            )
-
-                        }
-                    },
-                    {
-                        name: '取消'
-                    }
-                ]
-            })
-
-        });
-
-        /**
-         * 将全部文章下架
-         */
-        $(".down-all").on("click", function () {
-            jeBox.open({
-                cell:"jbx",
-                title:"提示",
-                padding:"25px 10px",
-                content:'<div class="jeBox-iconbox jeicon1" style="text-align: center;">确定要将<span class="red">全部文章下架</span>吗？' +
-                '<br><span>下架前请先<span class="red">清空</span>删除确认！</span></div>',
-                maskLock : true ,
-                btnAlign:"center",
-                button:[
-                    {
-                        name: '下架',
-                        callback: function(index){
-                            jeBox.close(index);
-                            MD.ajax_post({url: "admin/article/undercarriage_all", dataType: 'text'}, function (res) {
-
-                                jeBox.msg('全部文章已下架', {icon: 2,time:1.5});
-                                setTimeout(function () {
-                                    location.reload();
-                                },1200)
-
-                            })
-                        }
-                    },
-                    {
-                        name: '取消'
-                    }
-                ]
-            })
-        })
-
-        /**
-         * 全部恢复
-         */
-        $(".up-all").on("click", function () {
-            jeBox.open({
-                cell:"jbx",
-                title:"提示",
-                padding:"25px 10px",
-                content:'<div style="text-align: center;">确定要将全部文章恢复吗？</div>',
-                maskLock : true ,
-                btnAlign:"center",
-                button:[
-                    {
-                        name: '恢复',
-                        callback: function(index){
-                            jeBox.close(index);
-                            MD.ajax_post({url: "admin/article/topcarriage_all", dataType: 'text'}, function (res) {
-
-                                jeBox.msg('全部文章已恢复', {icon: 2,time:1.5});
-                                setTimeout(function () {
-                                    location.reload();
-                                },1200)
-
-                            })
-                        }
-                    },
-                    {
-                        name: '取消'
-                    }
-                ]
-            })
         })
     })
 </script>
